9a38d9fc7848aa32dbdda91e53948593f07b2528
unknown <you@example.com>
2024-02-02 16:47:14 +0100

    controller

--- router/userRouter.ts
+++ router/userRouter.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
-import { createUser, updateUserLocation } from "../controller/userController";
+import { createUser, getAllUser, logOut, signinAll, updateUserLocation, verifyUser } from "../controller/userController";
 
 const router: Router = Router();
 
-router.route("/create-user").post(createUser);
-
-
 router.route('/create-user').post(createUser);
 router.route('/create-user-location').post(updateUserLocation);
+router.route("/sign-in").post(signinAll);
+router.route("/verify").patch(verifyUser);
+router.route("/logOut").get(logOut);
+router.route("/reset-password").get(getAllUser);
+router
 
 export default router;
 
