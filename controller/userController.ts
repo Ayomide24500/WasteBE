0a3bb621c6606b1998fe843f51a3271ee82a9ca7
unknown <you@example.com>
2024-02-02 15:04:32 +0100

    location update

--- controller/userController.ts
+++ controller/userController.ts
@@ -16,13 +16,47 @@ export const createUser = async(req:Request, res:Response):Promise<Response> =>{
       email,
       password:hashedPassword,
       token,
-      status: "user"
+      status: "user",
     });
     return res.status(HTTP.CREATED).json({
       message: 'user created successfully',
       data: user,
       status: HTTP.CREATED
     });
+  } catch (error) {
+    return res.status(HTTP.BAD).json({
+      message: 'error creating user',
+      status: HTTP.BAD
+    });
+  }
+};
+
+export const updateUserLocation = async(req:Request, res:Response):Promise<Response> =>{
+  try {
+    const {userID} = req.params;
+    
+
+    const user = await userModel.findById(userID);
+
+    if (user) {
+      const location = await userModel.findByIdAndUpdate(userID,{
+        address: "",
+      },
+      {new: true}
+      );
+      return res.status(HTTP.CREATED).json({
+      message: 'user created successfully',
+      data: location,
+      status: HTTP.CREATED
+    });
+    } else {
+      return res.status(HTTP.BAD).json({
+      message: 'error getting user ',
+      status: HTTP.BAD
+    });
+    }
+
+    
   } catch (error) {
     return res.status(HTTP.BAD).json({
       message: 'error creating user',
