e29281b296e20ee37b657c17dff41f361c4ba09c
Ayomide24500 <Ayomideadisa83@gmail.com>
2024-02-02 13:25:22 +0100

    first

---
+++ utils/interface.ts
@@ -0,0 +1,24 @@
+import { HTTP } from "./enums";
+
+export interface iError {
+  name: string;
+  message: string;
+  status: HTTP;
+  success: boolean;
+}
+
+export interface iUser {
+  email: string;
+  password: string;
+  token: string;
+  AdminCode: string;
+  status: string;
+  verify: boolean;
+  allpassword: any[];
+}
+
+export interface iUserData extends iUser, Document {}
+export interface iPassword {
+  password: string;
+}
+export interface iPasswordData extends iPassword, Document {}
